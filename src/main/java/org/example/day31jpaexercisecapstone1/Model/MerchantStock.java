2b4f14b49075f57e050711519562229bdf2efea2
Faisal <faisalabdulazizz@outlook.com>
2025-08-04 10:03:20 +0300

    fixed some bugs..

--- src/main/java/org/example/day31jpaexercisecapstone1/Model/MerchantStock.java
+++ src/main/java/org/example/day31jpaexercisecapstone1/Model/MerchantStock.java
@@ -2,14 +2,12 @@ package org.example.day31jpaexercisecapstone1.Model;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.Min;
-import jakarta.validation.constraints.NotEmpty;
+
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
+
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.Check;
 
 import java.time.LocalDate;
 
